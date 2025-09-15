f85b775fb56a527facef6175dbba4a5328eed90d
Goktug Dulger <63787080+Deadstronaut@users.noreply.github.com>
2025-09-16 01:37:05 +0300

    Update BiRefNet_config.py

--- scripts/models/BiRefNet_dynamic/BiRefNet_config.py
+++ scripts/models/BiRefNet_dynamic/BiRefNet_config.py
@@ -1,11 +1,16 @@
-from transformers import PretrainedConfig
+# scripts/models/BiRefNet_dynamic/BiRefNet_config.py
+
+import torch
+
+try:
+    from transformers import PretrainedConfig
+except ImportError:
+    class PretrainedConfig:
+        pass
 
 class BiRefNetConfig(PretrainedConfig):
     model_type = "SegformerForSemanticSegmentation"
-    def __init__(
-        self,
-        bb_pretrained=False,
-        **kwargs
-    ):
+
+    def __init__(self, bb_pretrained=False, **kwargs):
         self.bb_pretrained = bb_pretrained
         super().__init__(**kwargs)
